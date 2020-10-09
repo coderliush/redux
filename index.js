37793663ed6ede9bbde79063da1ea7fdf44cfab6
liushuhao <794516020@qq.com>
2020-10-09 15:10:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: liushuhao
+ * @Date: 2020-10-09 14:27:01
+ * @LastEditors: liushuhao
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return 'init'
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App />, mountNode);
